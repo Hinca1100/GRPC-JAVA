006688b16eb52fb04f22507460c214431e56c999
Hinca1100 <JHINCA6@bancodebogota.com.co>
2023-09-21 20:25:59 -0500

    fix

--- docker/entrypoint.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-java -Djava.security.egd=file:/dev/./urandom -Xms256m -Xmx1024m  -jar /deployments/quarkus-run.jar
-echo '**** Launching application ****'
--- docker/entrypoint_native.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-./application
-echo '**** Launching application ****'
