64171bdacbac477a8b5396f81d56be7deb476723
Hinca1100 <JHINCA6@bancodebogota.com.co>
2023-09-27 11:28:36 -0500

    fix

--- target/quarkus-grpc5534981260413142801.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-"/Library/Java/JavaVirtualMachines/adoptopenjdk-11.jdk/Contents/Home/bin/java" -cp "/Users/jhinca6/.m2/repository/io/quarkus/quarkus-grpc-protoc-plugin/3.2.0.Final/quarkus-grpc-protoc-plugin-3.2.0.Final-shaded.jar" io.quarkus.grpc.protoc.plugin.MutinyGrpcGenerator
--- target/quarkus-grpc7150784054470295004.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-"/Users/jhinca6/.vscode/extensions/redhat.java-1.16.0-darwin-arm64/jre/17.0.6-macosx-aarch64/bin/java" -cp "/Users/jhinca6/.m2/repository/io/quarkus/quarkus-grpc-protoc-plugin/3.2.0.Final/quarkus-grpc-protoc-plugin-3.2.0.Final-shaded.jar" io.quarkus.grpc.protoc.plugin.MutinyGrpcGenerator
